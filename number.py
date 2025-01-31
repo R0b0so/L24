35721dbe17608e216a3578d8280c91ac619e6ea8
R0b0so <ethansagar222@gmail.com>
2025-01-31 15:17:26 -0800

    hello

---
+++ number.py
@@ -0,0 +1,20 @@
+import random
+number = random.randint(1, 100)
+name = input("What is your name? ")
+counter = 1
+print("Hello",name,"Lets play a game,")
+if number % 2 == 0: 
+    print("The number is even")
+else:
+    print("The number is odd")
+while True:
+ guess = int(input("\nGuess a number from 1-100 "))
+ if guess < number:
+    counter = counter + 1
+    print("Guess is too low")
+ elif guess > number:
+    counter = counter + 1
+    print("Guess is too high")
+ elif guess == number:
+    print("Congrats,",name,"you found the number in",counter,"attempts")
+    break
